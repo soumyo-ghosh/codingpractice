3c31746a173a941f45de03faf0059917d9e60e1b
soumyo-ghosh <soumyo.sghosh@gmail.com>
2021-10-16 10:51:36 +0530

    List updated

--- pandas/List.md
+++ pandas/List.md
@@ -1,3 +1,4 @@
 # This is a list of the excercises
 
 1. Create a simple series using pandas
+2. Convert a pandas module series to python list
---
+++ pandas/pandas2.py
@@ -0,0 +1,15 @@
+#Convert a pandas module series to python list
+
+import pandas as pd
+
+ds=pd.Series([34,56,72,32,1])
+
+ls=ds.tolist()
+
+print("This is a series:")
+print(ds)
+print(type(ds))
+
+print("This is a list")
+print(ls)
+print(type(ls))
